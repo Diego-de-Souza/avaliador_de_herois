9cd1d94ebbc26d02298d0243016acc8c664d85fa
Diego de Souza Lima <diegodesouza.souza@gmail.com>
2025-06-20 11:08:57 -0300

    adicao de pligin do tinymce

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
 bootstrapApplication(AppComponent, appConfig)
   .then(() => {
     if ('serviceWorker' in navigator) {
