e0f24cabda1b7d7a10a7e4f5b6fe074322fbd52e
Diego de Souza Lima <diegodesouza.souza@gmail.com>
2025-06-20 13:08:49 -0300

    acerto rota imagem

--- src/app/features/plataforma/view-artigos/view-artigos.component.ts
+++ src/app/features/plataforma/view-artigos/view-artigos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { HeaderPlatformComponent } from '../../../shared/components/header-platform/header-platform.component';
 
 @Component({
   selector: 'app-view-artigos',
   standalone: true,
-  imports: [],
+  imports: [HeaderPlatformComponent],
   templateUrl: './view-artigos.component.html',
   styleUrl: './view-artigos.component.css'
 })
