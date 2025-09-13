d9682c94eea2d2df0bee066af03b1198ff277bd9
Diego de Souza Lima <diegodesouza.souza@gmail.com>
2025-09-13 11:12:32 -0300

    improve(app): mudancas no app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MessageService } from './core/service/message/message.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, NgbModule],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
